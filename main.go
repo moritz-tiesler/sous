0bb0b6770f9f5c0b8cbaae23a0e133d3dd2e9277
Moritz Tiesler <moritz.tiesler@t-online.de>
2025-07-03 14:28:22 +0200

    switch from generate to chat mode

--- main.go
+++ main.go
@@ -14,25 +14,56 @@ func main() {
 		log.Fatal(err)
 	}
 
-	req := &api.GenerateRequest{
+	req := &api.ChatRequest{
 		// Model:  "gemma2",
-		Model:  "devstral:24b-small-2505-q8_0",
-		Prompt: "how many planets are there?",
+		Model: "devstral:24b-small-2505-q8_0",
+		Messages: []api.Message{
+			{
+				Role:    "user",
+				Content: "how many planets are there?",
+			},
+		},
 
 		// set streaming to false
 		Stream: new(bool),
 	}
 
 	ctx := context.Background()
-	respFunc := func(resp api.GenerateResponse) error {
+	respFunc := func(resp api.ChatResponse) error {
 		// Only print the response here; GenerateResponse has a number of other
 		// interesting fields you want to examine.
-		fmt.Println(resp.Response)
+		fmt.Println(resp.Message.Content)
 		return nil
 	}
 
-	err = client.Generate(ctx, req, respFunc)
+	// scanner := bufio.NewScanner(os.Stdin)
+	// getUserMessage := func() (string, bool) {
+	// 	if !scanner.Scan() {
+	// 		return "", false
+	// 	}
+	// 	return scanner.Text(), true
+	// }
+
+	err = client.Chat(ctx, req, respFunc)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	// agent := NewAgent(client, getUserMessage)
 }
+
+func NewAgent(client *api.Client, getUserMessage func() (string, bool)) *Agent {
+	return &Agent{
+		client:         client,
+		getUserMessage: getUserMessage,
+	}
+}
+
+type Agent struct {
+	client         *api.Client
+	getUserMessage func() (string, bool)
+}
+
+// func (a *Agent) Run(ctx context.Context) error {
+// 	conversation := []
+// }
